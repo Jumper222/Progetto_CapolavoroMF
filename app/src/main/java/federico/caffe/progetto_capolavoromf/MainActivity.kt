b63a9f04e0d950410f59e1b5f386219da8ebce85
Francesco Lombardi <IlSaltatore222@gmail.com>
2024-04-25 11:06:16 +0200

    Viewbinding attivato, buttonPlay modifica textView.

--- app/src/main/java/federico/caffe/progetto_capolavoromf/MainActivity.kt
+++ app/src/main/java/federico/caffe/progetto_capolavoromf/MainActivity.kt
@@ -1,16 +1,37 @@
 package federico.caffe.progetto_capolavoromf
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import federico.caffe.progetto_capolavoromf.databinding.ActivityMainBinding
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    //Qua creiamo la classe di binding NomeFile.xml -> NomeFileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //NON SO CHE FACCIA STA ROBA PERO E NECESSARIA
+
+        binding.ButtonPlay.setOnClickListener {
+            Log.d("MainActivity", "CLICCATO!")
+            binding.textView.text = "12344aZS1à+è3"
+
+        } //Crea l' evento OnClick sull' elemento ID: ButtonPlay, Stampa un log e cambia il text di textView
+
+
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)

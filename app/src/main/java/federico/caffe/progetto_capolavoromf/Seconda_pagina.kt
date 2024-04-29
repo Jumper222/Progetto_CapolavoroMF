7bd2bc459c40801f78a528fb9baf1ed93400798c
Namir622 <marcomac2005@gmail.com>
2024-04-29 18:38:31 +0200

    Passaggio seconda_pagina

---
+++ app/src/main/java/federico/caffe/progetto_capolavoromf/Seconda_pagina.kt
@@ -0,0 +1,27 @@
+package federico.caffe.progetto_capolavoromf
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Seconda_pagina : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_seconda_pagina)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val input = intent.getStringExtra("EXTRA_MESSAGE")
+
+        val textView = findViewById<TextView>(R.id.prova_input).apply {
+            text = input
+        }
+    }
+}
